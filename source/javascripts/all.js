d700bcc895159f52d0c470647a2d18f972e127d6
Scott Lesser <scottwlesser@gmail.com>
2016-06-17 06:45:45 -0500

    move js to correct place. out of page. add correct title.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,29 @@
-// This is where it all goes :)
+$(document).ready(function() {
+  $('a').smoothScroll();
+
+  $('body').on('click', '#mobile-navigation:not(.open)', function() {
+    $('.overlay').show();
+    $('#mobile-navigation').addClass('open');
+  });
+
+  $('body').on('click', '#mobile-navigation.open, .overlay a', function() {
+    $('.overlay').hide();
+    $('#mobile-navigation').removeClass('open');
+  });
+
+  $(".dropdown-button").click(function() {
+    var $button, $menu;
+    $button = $(this);
+    $menu = $button.siblings(".dropdown-menu");
+    $menu.toggleClass("show-menu");
+  });
+
+  $('#menu sup').html(function(index, oldHtml) {
+    if(oldHtml.length == 1) {
+      return oldHtml + "0";
+    }
+    else {
+      return oldHtml;
+    }
+  });
+});
